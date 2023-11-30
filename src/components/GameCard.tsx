6989c55c15994f38251799825906041b5a8cef27
ankitraz <109873375+ankitraz@users.noreply.github.com>
2023-11-30 23:39:53 +0530

    cleanuup the game card

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,10 +1,16 @@
-import { Button, Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
+import {
+  Button,
+  Card,
+  CardBody,
+  HStack,
+  Heading,
+  Image,
+} from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
 import { IoIosArrowRoundForward } from "react-icons/io";
-
 
 interface Props {
   game: Game;
@@ -15,13 +21,17 @@ const GameCard = ({ game }: Props) => {
     <Card overflow="hidden" borderRadius={10}>
       <Image src={getCroppedImageUrl(game.background_image)}></Image>
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-
-        <HStack justifyContent="space-between">
+        <HStack marginBottom={3} justifyContent="space-between">
           <PlatformIconList game={game} />
           <CriticScore score={game.metacritic} />
         </HStack>
-        <Button rightIcon={<IoIosArrowRoundForward />} marginTop={4} colorScheme="gray" size="xs">
+        <Heading fontSize="2xl">{game.name}</Heading>
+        <Button
+          rightIcon={<IoIosArrowRoundForward />}
+          marginTop={4}
+          colorScheme="gray"
+          size="xs"
+        >
           Read More
         </Button>
       </CardBody>
