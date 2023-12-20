f65762b5f5d502f5282412ac5cc5e585ab0bb812
ankitraz <109873375+ankitraz@users.noreply.github.com>
2023-12-20 19:10:40 +0530

    more accurate title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,9 +2,12 @@ import { Text } from "@chakra-ui/react";
 import useAI from "../hooks/useAI";
 
 const Title = () => {
-  const prompt = "Give me a random fact about video games in 30 words with emoji.";
+  const prompt = "Give me a random fact about video games in 30 words with emoji. also do not repeat a fact.";
 
   const { data } = useAI(prompt);
+
+  
+  
 
   return <Text display={{ base: "none", md: "block" }}>{data}</Text>;
 };
