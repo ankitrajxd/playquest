58f8109b4b777d641340e17c8d5cb3f17b7fc595
ankitraz <109873375+ankitraz@users.noreply.github.com>
2023-12-20 12:18:44 +0530

    added a spinner

--- src/components/AiDrawer.tsx
+++ src/components/AiDrawer.tsx
@@ -10,6 +10,7 @@ import {
   DrawerFooter,
   List,
   ListItem,
+  Spinner,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { GoogleGenerativeAI } from "@google/generative-ai";
@@ -22,17 +23,23 @@ const AiDrawer = () => {
   const genAI = new GoogleGenerativeAI(VITE_GOOGLE_AI_API_KEY);
 
   const [text, setText] = useState<string[]>([]);
+  const [isLoading, setIsloading] = useState<boolean>()
 
   useEffect(() => {
     async function run() {
       // For text-only input, use the gemini-pro model
       const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+
+
       const prompt =
         "Show me trending 5 video games in 2023. just give the games name and not the description";
+        setIsloading(true)
       const result = await model.generateContent(prompt);
       const response = await result.response;
       const resarray = response.text().split("\n");
       setText(resarray);
+      setIsloading(false)
     }
     run();
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -52,11 +59,12 @@ const AiDrawer = () => {
           <DrawerHeader borderBottomWidth="1px">Gemini AI</DrawerHeader>
 
           <DrawerBody>
-            <List>
+            {isLoading && <Spinner/>}
+            {isLoading === false && <List>
               {text.map((t, index) => (
                 <ListItem key={index}>{t}</ListItem>
               ))}
-            </List>
+            </List>}
           </DrawerBody>
 
           <DrawerFooter borderTopWidth="1px">
